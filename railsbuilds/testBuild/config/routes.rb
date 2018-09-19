8437f697bb171f82ff4923c3bf77793ca5c303fb
zitro <bryan@bryanortiz.com>
2018-09-19 10:19:36 -0400

    welcome.html.erb and route for index

--- railsbuilds/testBuild/config/routes.rb
+++ railsbuilds/testBuild/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :welcomes
+	get 'welcome/index'
+	resources :welcomes
   resources :tests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
