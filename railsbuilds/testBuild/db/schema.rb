8d7b7b33b22e47e5fc29a7a5ef52247fe74dbf0b
zitro <bryan@bryanortiz.com>
2018-09-19 10:31:23 -0400

    articles model

--- railsbuilds/testBuild/db/schema.rb
+++ railsbuilds/testBuild/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_19_141627) do
+ActiveRecord::Schema.define(version: 2018_09_19_143045) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tests", force: :cascade do |t|
     t.text "name"
