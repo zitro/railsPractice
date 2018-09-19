aad2d283940fc618e3ef8e76e6d88507bc1b8d06
zitro <bryan@bryanortiz.com>
2018-09-19 10:37:42 -0400

    articles index view, and controller update

--- railsbuilds/testBuild/config/routes.rb
+++ railsbuilds/testBuild/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	get 'welcomes/index'
 	root 'welcomes#index'
+	articles GET    /articles(.:format)
+	articles#index
 	resources :welcomes
   resources :tests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
